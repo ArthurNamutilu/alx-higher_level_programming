cbd11ca5dcbb603a1313c62ce1743363f4ec15fb
Arthur Namutilu <arthurggnamutilu@gmail.com>
2023-08-29 16:01:19 -0700

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square class - creating a private size attribute"""
+
+
+class Square:
+    """Square class with private instance attribute size"""
+    def __init__(self, size=0):
+    """ Initializing square class
+    Args:
+        size: size os square
+    Raise:
+        TypeError: if size not int
+        ValueError: if size less than 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
