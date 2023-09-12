a9ba90df2f07545846372d253b4075426db28bbc
Arthur Namutilu <arthurggnamutilu@gmail.com>
2023-09-12 07:02:42 -0700

    reading file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ Functions that opens a file """
     with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
-            print(line)
+            print(line, end="")
