1d1498b2f4d492df6dab4d74005cb2e9036ebacc
Arthur Namutilu <your.arthurggnamutilu@gmail.com>
2023-12-01 13:46:59 -0800

    rm sql scripts

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Makes table, the ID unique and default of 1
+CREATE TABLE IF NOT EXISTS unique_id (
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+);
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Script lists all the cities in a database
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id=states.id
+ORDER BY cities.id ASC;
