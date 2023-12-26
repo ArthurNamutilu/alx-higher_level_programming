adeb81398744a9de8d057bbe6f5f4ce79d98f320
Arthur Namutilu <your.arthurggnamutilu@gmail.com>
2023-12-26 07:28:36 -0800

    package

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Base class """
+
+
+class Base:
+    __nb_objects = 0  # class attribute
+
+    def __init__(self, id=None):
+        """ assigns id """
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
