febf9a7807866ac86880e6cc6264830895edc4e1
Arthur Namutilu <your.arthurggnamutilu@gmail.com>
2024-01-28 21:51:22 -0800

    js command line arguments

---
+++ 0x12-javascript-warm_up/arg.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+console.log(process.argv[0]);  // path to the Node.js executable
+console.log(process.argv[1]);  // path to the script that is being executed
+/*
+const length = process.argv.length - 2;
+if (length === 0) {
+  console.log('No argument');
+} else if (length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+*/
