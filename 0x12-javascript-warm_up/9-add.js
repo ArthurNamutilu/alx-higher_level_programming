a76c7f493862165cf9dee228c0609c2252f9d6d6
Arthur Namutilu <your.arthurggnamutilu@gmail.com>
2024-02-02 19:02:27 -0800

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   return parseInt(a) + parseInt(b);
 }
 
@@ -10,6 +10,6 @@ const num2 = parseInt(process.argv[3]);
 if (!isNaN(num1) && !isNaN(num2)) {
   const result = add(num1, num2);
   console.log(result);
-  } else {
-    console.log('NaN');
-  }
+} else {
+  console.log('NaN');
+}
