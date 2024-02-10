c87fe813c2d465e457bd788fd68f579df29d4b48
Arthur Namutilu <your.arthurggnamutilu@gmail.com>
2024-02-09 18:31:58 -0800

    retangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
+// JS script to define a class, creates an instance method to print rectangle
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== undefined typeof h !== undefined) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
       return this;
     } else {
       this.width = w;
       this.height = h;
     }
   }
-
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('x'.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 }
-
 module.exports = Rectangle;
