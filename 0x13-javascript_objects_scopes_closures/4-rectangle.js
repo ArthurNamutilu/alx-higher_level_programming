1907e7e1d23983986b59f6cf061d79efc3bccf40
Arthur Namutilu <your.arthurggnamutilu@gmail.com>
2024-02-09 19:33:03 -0800

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const x = this.height;
+    this.height = this.width;
+    this.width = x;
+    // this.print();
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+    // this.print();
+  }
+}
+module.exports = Rectangle;
